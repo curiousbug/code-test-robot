0dd270e7e86fbd51fa712be8e34020d09ca5639b
Allen Ling <lingallen@gmail.com>
2013-11-14 16:28:18 +1100

    Robot #place & #report added

--- Code/robot.rb
+++ Code/robot.rb
@@ -1,2 +1,40 @@
+require_relative 'tabletop'
+
 class Robot
+  def initialize tabletop
+    @tabletop = tabletop
+
+    @location = {
+      'x' => nil,
+      'y' => nil,
+    }
+
+    @compass = {
+      :north => "NORTH",
+      :east => "EAST",
+      :south => "SOUTH",
+      :west => "WEST",
+    }
+
+    @aspect = nil
+  end
+
+  def place x, y, aspect
+    if (x < @tabletop.get_x_max()) && (y < @tabletop.get_y_max()) && (@compass.invert[aspect] != nil)
+      @location['x'] = x
+      @location['y'] = y
+
+      @aspect = @compass.invert[aspect]
+    else
+      return
+    end
+  end
+
+  def report
+    if @location['x'] && @location['y'] && @compass[@aspect]
+      "Output: " + @location['x'].to_s() + ", " + @location['y'].to_s() + ", " + @compass[@aspect]
+    else
+      return ""
+    end
+  end
 end
--- Code/spec/robot_spec.rb
+++ Code/spec/robot_spec.rb
@@ -1,10 +1,40 @@
 require 'spec_helper'
 
 describe Robot do
+  before :each do
+    @tabletop = Tabletop.new(5, 5)
+    @robot = Robot.new(@tabletop)
+  end
+
   describe "#new" do
     it "returns a Robot object" do
-      @robot = Robot.new
       @robot.should be_an_instance_of Robot
+    end
+  end
+
+  describe "#report and #place" do
+    context "robot is off table:" do
+      it "robot has not been placed" do
+        @robot.report.should eql ""
+      end
+
+      it "robot has been placed in an invalid location" do
+        @robot.place(100, 100, "NORTH")
+        @robot.report.should eql ""
+      end        
+    end
+
+    context "robot is on the table:" do
+      it "robot has been placed in a valid location" do
+        @robot.place(0, 0, "NORTH")
+        @robot.report.should eql "Output: 0, 0, NORTH"
+      end
+
+      it "robot has been placed in an invalid location" do
+        @robot.place(0, 0, "NORTH")
+        @robot.place(100, 100, "NORTH")
+        @robot.report.should eql "Output: 0, 0, NORTH"
+      end
     end
   end
 end
