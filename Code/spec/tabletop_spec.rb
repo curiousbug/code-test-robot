a9af90373d1688b970fd86d9aee3fb049ba902b3
Allen Ling <lingallen@gmail.com>
2013-11-14 12:37:30 +1100

    Refactored Tabletop test to support further tests

--- Code/spec/tabletop_spec.rb
+++ Code/spec/tabletop_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe Tabletop do
+  before :each do
+    @tabletop = Tabletop.new(5, 5)
+  end
+
   describe "#new" do
     it "return a Tabletop object" do
-      @tabletop = Tabletop.new(5, 5)
       @tabletop.should be_an_instance_of Tabletop
     end
   end
